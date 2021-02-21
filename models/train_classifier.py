9782834a449b117d9ab40906a10d8c3c6bae88ba
Trujillo <heber.trj.urt@gmail.com>
2021-02-21 19:49:41 +0100

    'sqlite:///' str was added by defect SQL engine when calling create _engine function.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -40,7 +40,7 @@ def load_data(database_filepath: str):
     category_names: list
         list containing categories to be used
     """
-    engine = create_engine(database_filepath)
+    engine = create_engine('sqlite:///'+database_filepath)
     df = pd.read_sql_table("messages", engine)
     X = df['message'].copy()
     Y = df.drop(
