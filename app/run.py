86d94990d62ca39029429c29f82a3706f6dce2bf
Trujillo <heber.trj.urt@gmail.com>
2021-02-22 08:05:00 +0100

    new visuals were added

--- app/run.py
+++ app/run.py
@@ -1,29 +1,65 @@
 import json
 import plotly
 import pandas as pd
-
+import re
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Pie
 import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
-def tokenize(text):
+def tokenize(text: str):
+    """"
+    Clean and tokenize the text data
+
+    parameters
+    -----------
+    text: str
+        Long string to be cleaned and tokenize
+
+    returns
+    -------
+    clean_tokens: list
+        list of cleaned word tokens
+
+    """
+
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+
+    # replace each url in text string with placeholder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # lower case text
+    text = text.lower()
+
+    # Remove stop words and puntuation
     tokens = word_tokenize(text)
+    tokens = [re.sub(r"[^a-z0-9]", "", word) for word in tokens if word not in stopwords.words("english")]
+    tokens = [word for word in tokens if word != '']
+
+    # initiate lemmatizer
     lemmatizer = WordNetLemmatizer()
 
+    # iterate through each token
     clean_tokens = []
     for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        tok = tok.strip()
+        clean_tok = lemmatizer.lemmatize(lemmatizer.lemmatize(tok), pos='v')
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -42,15 +78,28 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    columns = df.drop(
+        labels=['id', 'message', 'original', 'genre'],
+        axis=1).columns
+
+    categories_freq = df[columns].sum() / df.shape[0]
+    categories_freq.sort_values(
+        ascending=False,
+        inplace=True)
+
+    values = categories_freq.values.tolist()
+    complement = list(map(lambda x: 1 - x, values))
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        # Graph 1
         {
             'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
+                Pie(
+                    labels=genre_names,
+                    values=genre_counts
                 )
             ],
 
@@ -63,7 +112,43 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        # graph 2
+        {
+            'data': [
+                Bar(
+                    x=categories_freq.index.tolist(),
+                    y=values,
+                    name='% Obs. Cases',
+                    marker=dict(
+                            color='rgb(255, 193, 37)'
+                                )
+                ),
+                Bar(
+                    x=categories_freq.index.tolist(),
+                    y=complement,
+                    name='',
+                    hoverinfo='none',
+                    marker=dict(
+                            color='rgb(255, 193, 37)',
+                            opacity=0.3
+                                )
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message by Category',
+                'yaxis': {
+                    'title': "frequency"
+                },
+                'xaxis': {
+                    'title': "Category"
+                },
+                'barmode' : 'stack'
+            }
         }
+
+
     ]
     
     # encode plotly graphs in JSON
